9f6a213372eb3256eae0300d40d237976a13b66a
xob0t <32616886+xob0t@users.noreply.github.com>
2024-01-03 10:52:08 +0300

    new functions

--- google_photos_cleanup_tools.user.js
+++ google_photos_cleanup_tools.user.js
@@ -15,11 +15,7 @@
 
 
 
-const getMedia = async (timestamp = null) => {
-    // Retrieve media items created before the provided timestamp
-    const rpcids = "lcxiM";
-    const limit = 500; // 500 max
-    const requestData = [[[rpcids, `[null,${timestamp},${limit},null,1,1,${timestamp}]`, null, "generic"]]];
+const makeApiRequest = async (rpcids, requestData) => {
     const requestDataString = `f.req=${encodeURIComponent(JSON.stringify(requestData))}&at=${encodeURIComponent(window.WIZ_global_data.SNlM0e)}&`;
     const url = `https://photos.google.com${window.WIZ_global_data.eptZe}data/batchexecute?rpcids=${rpcids}&f.sid=${window.WIZ_global_data.FdrFJe}`;
     try {
@@ -38,7 +34,22 @@ const getMedia = async (timestamp = null) => {
         parsedResponse = JSON.parse(parsedResponse[0][2]);
         return parsedResponse;
     } catch (error) {
-        console.error('getMedia Error:', error);
+        console.error(`Error in ${rpcids} request:`, error);
+        throw error;
+    }
+};
+
+
+const getMediaAPI = async (timestamp = null) => {
+    // Retrieve media items created before the provided timestamp
+    const rpcids = "lcxiM";
+    const limit = 500; // 500 max
+    const requestData = [[[rpcids, `[null,${timestamp},${limit},null,1,1,${timestamp}]`, null, "generic"]]];
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0];
+    } catch (error) {
+        console.error('Error in getMedia:', error);
         throw error;
     }
 };
@@ -80,71 +91,84 @@ const parseMediaPage = (mediaPage) => {
 
 };
 
-const getAlbums = async () => {
+const getAlbumsAPI = async () => {
     const rpcids = "Z5xsfc";
-    const requestData = [[[rpcids,"[null,null,null,null,1,null,null,100]",null,"1"]]];
-
-    const requestDataString = `f.req=${encodeURIComponent(JSON.stringify(requestData))}&at=${encodeURIComponent(window.WIZ_global_data.SNlM0e)}&`;
-    const url = `https://photos.google.com${window.WIZ_global_data.eptZe}data/batchexecute?rpcids=${rpcids}&f.sid=${window.WIZ_global_data.FdrFJe}`;
+    const requestData = [[[rpcids, "[null,null,null,null,1,null,null,100]",null,"1"]]]
     try {
-        const response = await fetch(url, {
-            "headers": {
-                "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-            },
-            "body": requestDataString,
-            "method": "POST",
-            "credentials": "include"
-        });
-
-        const responseBody = await response.text();
-        const jsonResponse = responseBody.substring(responseBody.indexOf('['));
-        let parsedResponse = JSON.parse(jsonResponse);
-        parsedResponse = JSON.parse(parsedResponse[0][2]);
-        return parsedResponse;
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0];
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error in getAlbums:', error);
         throw error;
     }
 };
 
-fetch("https://photos.google.com/u/2/_/PhotosUi/data/batchexecute?rpcids=Z5xsfc&source-path=%2Fu%2F2%2Falbums&f.sid=4036024624387355762&bl=boq_photosuiserver_20231217.04_p1&hl=ru&soc-app=165&soc-platform=1&soc-device=1&_reqid=1433634&rt=c", {
-  "headers": {
-    "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-  },
-  "body": "f.req=%5B%5B%5B%22Z5xsfc%22%2C%22%5Bnull%2Cnull%2Cnull%2Cnull%2C1%2Cnull%2Cnull%2C100%5D%22%2Cnull%2C%221%22%5D%5D%5D&at=AMte4lXSzqC_RcmbRKeFC-JrTeJ4%3A1704262834098&",
-  "method": "POST",
-  "credentials": "include"
-});
+const getTrashItemsAPI = async () => {
+    const rpcids = "zy0IHe";
+    const requestData = [[[rpcids,"[]",null,"1"]]]
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0];
+    } catch (error) {
+        console.error('Error in getTrashItems:', error);
+        throw error;
+    }
+};
 
-// restore from trash
-// fetch("https://photos.google.com/u/2/_/PhotosUi/data/batchexecute?rpcids=XwAOJf&source-path=%2Fu%2F2%2Ftrash&f.sid=7197950484839168837&bl=boq_photosuiserver_20231217.04_p1&hl=ru&soc-app=165&soc-platform=1&soc-device=1&_reqid=7911303&rt=c", {
-//   "headers": {
-//     "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-//   },
-//   "body": "f.req=%5B%5B%5B%22XwAOJf%22%2C%22%5Bnull%2C3%2C%5B%5C%22eUcTwnOSjTnE29ORD8Fa9-Kihtg%5C%22%5D%2C2%5D%22%2Cnull%2C%22generic%22%5D%5D%5D&at=AMte4lWBMNjJ3vZrPef9y9QiePd5%3A1704240502161&",
-//   "method": "POST",
-//   "credentials": "include"
-// });
+const restoreFromTrashAPI = async (mediaIdList) => {
+    const rpcids = "XwAOJf";
+    const requestData = [[[rpcids,`[null,3,${JSON.stringify(mediaIdList)},2]`,null,"generic"]]]
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0];
+    } catch (error) {
+        console.error('Error in restoreFromTrash:', error);
+        throw error;
+    }
+};
 
-// get trash items
-//fetch("https://photos.google.com/u/2/_/PhotosUi/data/batchexecute?rpcids=zy0IHe&source-path=%2Fu%2F2%2Ftrash&f.sid=7197950484839168837&bl=boq_photosuiserver_20231217.04_p1&hl=ru&soc-app=165&soc-platform=1&soc-device=1&_reqid=7811303&rt=c", {
-//   "headers": {
-//     "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-//   },
-//   "body": "f.req=%5B%5B%5B%22zy0IHe%22%2C%22%5B%5D%22%2Cnull%2C%221%22%5D%5D%5D&at=AMte4lWBMNjJ3vZrPef9y9QiePd5%3A1704240502161&",
-//   "method": "POST",
-//   "credentials": "include"
-// });
+const addMediaToAlbumAPI = async (productIdList, albumName = null, albumId = null) => {
+    // supply album ID for moving into existing album, or a name for a new one
+    const rpcids = "E1Cajb";
+    let requestData = ''
+    if (albumName){
+        requestData = [[[rpcids,`[${JSON.stringify(productIdList)},null,\"${albumName}\"]`,null,"generic"]]]
+    }
+    else if (albumId){
+        requestData = [[[rpcids,`[${JSON.stringify(productIdList)},\"${albumId}\"]`,null,"generic"]]]
+    }
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0];
+    } catch (error) {
+        console.error('Error in restoreFromTrash:', error);
+        throw error;
+    }
+};
 
-// add media to a new album
-//fetch("https://photos.google.com/u/2/_/PhotosUi/data/batchexecute?rpcids=E1Cajb&source-path=%2Fu%2F2%2F&f.sid=8553615170827615654&bl=boq_photosuiserver_20231217.04_p1&hl=ru&soc-app=165&soc-platform=1&soc-device=1&_reqid=4609404&rt=c", {
-//   "headers": {
-//     "content-type": "application/x-www-form-urlencoded;charset=UTF-8"
-//   },
-//   "body": "f.req=%5B%5B%5B%22E1Cajb%22%2C%22%5B%5B%5C%22AF1QipNxQhUzwtBz3szRApVYsBmHkTyREPb4KKwW3hI%5C%22%2C%5C%22AF1QipML49wK1ZcZdRGkqREkF6KEINMbjbgOjx7S1_M%5C%22%2C%5C%22AF1QipOu7e1cFa_SCsPgEBaBfhuvlR8ouj-G4EFxbNw%5C%22%2C%5C%22AF1QipO-rQswDWZvH4mfRdCA7u4CVmHlCCasv2A2aN8%5C%22%5D%2Cnull%2C%5C%22%D0%91%D0%B5%D0%B7%20%D0%BD%D0%B0%D0%B7%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D&at=AMte4lW0eRZi7xC8Jue2nBlEbxwQ%3A1704238604091&",
-//   "method": "POST",
-//   "credentials": "include"
-// });
+const addMediaToAlbum = async (name, productIdList) => {
+    // check if there is an existing almum with the supplied name
+    // if no albums match, create a new one
+    const findAlbumByName = (albumList, searchString) => {
+        const matchingObject = albumList.find(obj =>
+            obj[8][72930366][1] === searchString
+        );
+        return matchingObject || null;
+      }
+
+    const albumList = await getAlbumsAPI()
+    const targetAlbum = findAlbumByName(albumList, name)
+    if (targetAlbum){
+        console.log("adding media to an exisitng album")
+        addMediaToAlbumAPI(albumId = targetAlbum[0], productIdList)
+    }
+    else{
+        console.log("adding media to a new album")
+        addMediaToAlbumAPI(albumName = name, productIdList)
+    }
+    
+    
+}
 
 // get media info by id
 // fetch("https://photos.google.com/u/0/_/PhotosUi/data/batchexecute?rpcids=fDcn4b&source-path=%2Fu%2F0%2Fphoto%2FAF1QipODG_3h9KPwBXVJHaRxu7RTOK1bi52vtOTFWCGQ&f.sid=1480257426694425772&bl=boq_photosuiserver_20231217.04_p1&hl=ru&soc-app=165&soc-platform=1&soc-device=1&_reqid=1640372&rt=c", {
@@ -157,90 +181,59 @@ fetch("https://photos.google.com/u/2/_/PhotosUi/data/batchexecute?rpcids=Z5xsfc&
 //     "credentials": "include"
 // });
 
-const getMediaSize = async (mediaPage) => {
-    // Retrieve media items created before the provided timestamp
+const getMediaSizeAPI = async (mediaPage) => {
     const rpcids = "EWgK9e";
     const extractedIDs = mediaPage[0].map(id => [id[0]]);
     const resultString = JSON.stringify(extractedIDs);
 
-    const requestData = [[[rpcids,`[[[${resultString}],[[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[],null,null,null,null,null,null,null,null,null,null,[]]]]]`,null,"generic"]]];
+    const requestData = [[[rpcids, `[[[${resultString}],[[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[],null,null,null,null,null,null,null,null,null,null,[]]]]]`, null, "generic"]]];
 
-    const requestDataString = `f.req=${encodeURIComponent(JSON.stringify(requestData))}&at=${encodeURIComponent(window.WIZ_global_data.SNlM0e)}&`;
-    const url = `https://photos.google.com${window.WIZ_global_data.eptZe}data/batchexecute?rpcids=${rpcids}&f.sid=${window.WIZ_global_data.FdrFJe}`;
     try {
-        const response = await fetch(url, {
-            "headers": {
-                "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-            },
-            "body": requestDataString,
-            "method": "POST",
-            "credentials": "include"
-        });
-
-        const responseBody = await response.text();
-        const jsonResponse = responseBody.substring(responseBody.indexOf('['));
-        let parsedResponse = JSON.parse(jsonResponse);
-        parsedResponse = JSON.parse(parsedResponse[0][2])[0][1];
-        return parsedResponse;
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0][1];
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error in getMediaSize:', error);
         throw error;
     }
 };
 
-const moveMediaToTrash = async (mediaIdList) => {
+const moveMediaToTrashAPI = async (mediaIdList) => {
     const rpcids = "XwAOJf";
     const requestData = [[[rpcids, `[null,1,${JSON.stringify(mediaIdList)},3]`, null, "generic"]]];
-
-    const requestDataString = `f.req=${encodeURIComponent(JSON.stringify(requestData))}&at=${encodeURIComponent(window.WIZ_global_data.SNlM0e)}&`;
-    const url = `https://photos.google.com${window.WIZ_global_data.eptZe}data/batchexecute?rpcids=${rpcids}&f.sid=${window.WIZ_global_data.FdrFJe}`;
-
     try {
-        const response = await fetch(url, {
-            "headers": {
-                "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-            },
-            "body": requestDataString,
-            "method": "POST",
-            "credentials": "include"
-        });
-
-        const responseBody = await response.text();
-        const jsonResponse = responseBody.substring(responseBody.indexOf('['));
-        let parsedResponse = JSON.parse(jsonResponse);
-        parsedResponse = JSON.parse(parsedResponse[0][2]);
-        return parsedResponse;
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0][2];
     } catch (error) {
-        console.error('Error:', error);
+        console.error('Error in moveMediaToTrash:', error);
         throw error;
     }
 };
 
-async function logMediaData() {
-    const mediaData = await getMedia();
-    console.log('getMedia:', mediaData);
+async function logForTesting() {
+    const mediaData = await addMediaToAlbum("newalbum1", ["AF1QipOu7e1cFa_SCsPgEBaBfhuvlR8ouj-G4EFxbNw","AF1QipPn_s9KnBp86EFUh3aDsj0vl06IVpXQHmn5AyE", "AF1QipMqTOLq6pDeqMsioQZ-aU6urM-fCGVuw9mfsOA"]);
+    console.log('addMediaToaNewAlbum:', mediaData);
 }
 
 const deleteMediaTakingUpSpace = async () => {
     let nextTimestamp = null;
     console.log("deleteMediaTakingUpSpace")
-    while (true){
-        const mediaPage = await getMedia(nextTimestamp)
-        nextTimestamp = parseInt(mediaPage[2])-1
+    while (true) {
+        const mediaPage = await getMediaAPI(nextTimestamp)
+        nextTimestamp = parseInt(mediaPage[2]) - 1
         console.log('getMedia:', mediaPage);
-        if (mediaPage && !mediaPage[0]){
+        if (mediaPage && !mediaPage[0]) {
             console.log("no media to delete")
             return
         }
-        const size = await getMediaSize(mediaPage)
+        const size = await getMediaSizeAPI(mediaPage)
         console.log(size)
 
         console.log("page deleted")
     }
 }
+logForTesting()
 
 
-logMediaData()
-window.logMediaData = logMediaData;
+window.logForTesting = logForTesting;
 //deleteMediaTakingUpSpace()
 

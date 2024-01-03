cd206185fe1004df7388e477f9a5b3cb494d6ad1
xob0t <32616886+xob0t@users.noreply.github.com>
2024-01-03 15:33:23 +0300

    kinda good

--- google_photos_cleanup_tools.user.js
+++ google_photos_cleanup_tools.user.js
@@ -40,14 +40,14 @@ const makeApiRequest = async (rpcids, requestData) => {
 };
 
 
-const getMediaAPI = async (timestamp = null) => {
+const getMediaPageAPI = async (timestamp = null) => {
     // Retrieve media items created before the provided timestamp
     const rpcids = "lcxiM";
-    const limit = 500; // 500 max
-    const requestData = [[[rpcids, `[null,${timestamp},${limit},null,1,1,${timestamp}]`, null, "generic"]]];
+    const limit = 500; // 500 max, if null returns 300
+    const requestData = [[[rpcids, JSON.stringify([null, timestamp, limit, null, 1, 1, timestamp]), null, "generic"]]];
     try {
         const response = await makeApiRequest(rpcids, requestData);
-        return response[0];
+        return response;
     } catch (error) {
         console.error('Error in getMedia:', error);
         throw error;
@@ -91,9 +91,49 @@ const parseMediaPage = (mediaPage) => {
 
 };
 
+const getTrashItemsAPI = async (pageId = null) => {
+    const rpcids = "zy0IHe";
+    const requestData = [[[rpcids, JSON.stringify([pageId]), null, "1"]]]
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response;
+    } catch (error) {
+        console.error('Error in getTrashItems:', error);
+        throw error;
+    }
+};
+
+const restoreFromTrashAPI = async (mediaIdList) => {
+    const rpcids = "XwAOJf";
+    const requestData = [[[rpcids, JSON.stringify([null, 3, mediaIdList, 2]), null, "generic"]]]
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0];
+    } catch (error) {
+        console.error('Error in restoreFromTrash:', error);
+        throw error;
+    }
+};
+
+const restoreAllFromTrash = async () => {
+    console.log("restoring trash");
+    let trashItems, nextPageId = null;
+    const restorePromises = [];
+    do {
+        [trashItems, nextPageId] = await getTrashItemsAPI(nextPageId);
+        const mediaIdList = trashItems.map(item => item[3]);
+        const restorePromise = restoreFromTrashAPI(mediaIdList);
+        restorePromises.push(restorePromise);
+
+    } while (nextPageId);
+
+    await Promise.all(restorePromises);
+    console.log("trash restored")
+};
+
 const getAlbumsAPI = async () => {
     const rpcids = "Z5xsfc";
-    const requestData = [[[rpcids, "[null,null,null,null,1,null,null,100]",null,"1"]]]
+    const requestData = [[[rpcids, "[null,null,null,null,1,null,null,100]", null, "1"]]]
     try {
         const response = await makeApiRequest(rpcids, requestData);
         return response[0];
@@ -103,90 +143,119 @@ const getAlbumsAPI = async () => {
     }
 };
 
-const getTrashItemsAPI = async () => {
-    const rpcids = "zy0IHe";
-    const requestData = [[[rpcids,"[]",null,"1"]]]
-    try {
-        const response = await makeApiRequest(rpcids, requestData);
-        return response[0];
-    } catch (error) {
-        console.error('Error in getTrashItems:', error);
-        throw error;
-    }
-};
-
-const restoreFromTrashAPI = async (mediaIdList) => {
-    const rpcids = "XwAOJf";
-    const requestData = [[[rpcids,`[null,3,${JSON.stringify(mediaIdList)},2]`,null,"generic"]]]
-    try {
-        const response = await makeApiRequest(rpcids, requestData);
-        return response[0];
-    } catch (error) {
-        console.error('Error in restoreFromTrash:', error);
-        throw error;
-    }
-};
-
 const addMediaToAlbumAPI = async (productIdList, albumName = null, albumId = null) => {
     // supply album ID for moving into existing album, or a name for a new one
     const rpcids = "E1Cajb";
     let requestData = ''
-    if (albumName){
-        requestData = [[[rpcids,`[${JSON.stringify(productIdList)},null,\"${albumName}\"]`,null,"generic"]]]
+
+    if (albumName) {
+        requestData = [[[rpcids, JSON.stringify([productIdList, null, albumName]), null, "generic"]]]
     }
-    else if (albumId){
-        requestData = [[[rpcids,`[${JSON.stringify(productIdList)},\"${albumId}\"]`,null,"generic"]]]
+    else if (albumId) {
+        requestData = [[[rpcids, JSON.stringify([productIdList, albumId]), null, "generic"]]]
     }
     try {
         const response = await makeApiRequest(rpcids, requestData);
         return response[0];
     } catch (error) {
-        console.error('Error in restoreFromTrash:', error);
+        console.error('Error in addMediaToAlbumAPI:', error);
         throw error;
     }
 };
 
-const addMediaToAlbum = async (name, productIdList) => {
-    // check if there is an existing almum with the supplied name
-    // if no albums match, create a new one
-    const findAlbumByName = (albumList, searchString) => {
-        const matchingObject = albumList.find(obj =>
-            obj[8][72930366][1] === searchString
-        );
-        return matchingObject || null;
-      }
-
+const findAlbumByName = async (albumName) => {
+    // returns the first album that matches the given name
     const albumList = await getAlbumsAPI()
-    const targetAlbum = findAlbumByName(albumList, name)
-    if (targetAlbum){
-        console.log("adding media to an exisitng album")
-        addMediaToAlbumAPI(albumId = targetAlbum[0], productIdList)
-    }
-    else{
-        console.log("adding media to a new album")
-        addMediaToAlbumAPI(albumName = name, productIdList)
-    }
-    
-    
+    const matchingObject = albumList.find(obj =>
+        obj[8][72930366][1] === albumName
+    );
+    return matchingObject || null;
 }
 
-// get media info by id
-// fetch("https://photos.google.com/u/0/_/PhotosUi/data/batchexecute?rpcids=fDcn4b&source-path=%2Fu%2F0%2Fphoto%2FAF1QipODG_3h9KPwBXVJHaRxu7RTOK1bi52vtOTFWCGQ&f.sid=1480257426694425772&bl=boq_photosuiserver_20231217.04_p1&hl=ru&soc-app=165&soc-platform=1&soc-device=1&_reqid=1640372&rt=c", {
-//     "headers": {
-//         "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-//     },
-//     "body": "f.req=%5B%5B%5B%22fDcn4b%22%2C%22%5B%5C%22AF1QipODG_3h9KPwBXVJHaRxu7RTOK1bi52vtOTFWCGQ%5C%22%2C1%5D%22%2Cnull%2C%221%22%5D%5D%5D&at=AMte4lXI-nfAuvjpeDZpLWR_q1Pb%3A1704183173180&",
-//     "method": "POST",
-//     "mode": "cors",
-//     "credentials": "include"
-// });
+const addMediaToAlbum = async (albumName, productIdList) => {
+    // check if there is an existing album with the given name
+    // if no matching album found, create a new one
+    const targetAlbum = await findAlbumByName(albumName)
+    if (targetAlbum) {
+        console.log("adding media to an exisitng album", targetAlbum)
+        addMediaToAlbumAPI(productIdList, null, targetAlbum[0])
+    }
+    else {
+        console.log("adding media to a new album")
+        addMediaToAlbumAPI(productIdList, albumName)
+    }
+};
 
-const getMediaSizeAPI = async (mediaPage) => {
+const getMediaInfoAPI = async (productId) => {
+    // get media info for a single media item by its product id
+    const rpcids = "fDcn4b";
+    const requestData = [[[rpcids, JSON.stringify([productId, 1]), null, "1"]]];
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response[0];
+    } catch (error) {
+        console.error('Error in getMediaInfoAPI:', error);
+        throw error;
+    }
+};
+
+const getAlbumMediaItemsAPI = async (albumId, pageId = null) => {
+    // get media info for a single media by its product id
+    const rpcids = "snAcKc";
+    const requestData = [[[rpcids, JSON.stringify([albumId, pageId, null, null, 1]), null, "generic"]]]
+    try {
+        const response = await makeApiRequest(rpcids, requestData);
+        return response;
+    } catch (error) {
+        console.error('Error in getAlbumMediaItems:', error);
+        throw error;
+    }
+};
+
+const trashAllMediaInAlbum = async (albumName) => {
+    console.log(`deleting all media in the album ${albumName}`);
+    const targetAlbum = await findAlbumByName(albumName);
+    let nextPageId = null;
+    const promises = [];
+    do {
+        const albumPage = await getAlbumMediaItemsAPI(targetAlbum[0], nextPageId);
+        nextPageId = albumPage[2]
+        const mediaIdList = albumPage[1].map(item => item[3]);
+        const promise = moveMediaToTrashAPI(mediaIdList);
+        promises.push(promise);
+
+    } while (nextPageId);
+
+    await Promise.all(promises);
+    console.log("all media in the album moved to trash")
+};
+
+const identifyMediaOccupingSpace = async () => {
+    // scans all library for media that occupies space
+    // adds it to a "Occupies Space" album
+    console.log("scanning for media occuping space");
+    const promises = [];
+    let mediaPage, timestamp = null;
+    do {
+        mediaPage = await getMediaPageAPI(timestamp);
+        timestamp = parseInt(mediaPage[2]) - 1
+        const productIdList = mediaPage[0].map(item => item[0]);
+        const sizeData = await getMediaSizeAPI(productIdList)
+        let idsOfMediaOccupingSpace = sizeData.filter(item => item[1][34][1] === 0).map(item => item[0]);
+        const restorePromise = addMediaToAlbum("Occupies Space", idsOfMediaOccupingSpace);
+        promises.push(restorePromise);
+
+    } while (timestamp);
+    await Promise.all(promises);
+    console.log("all media occuping space added to \"Occupies Space\" album")
+};
+
+const getMediaSizeAPI = async (productIdList) => {
+    // returns filename, size, occupied space
     const rpcids = "EWgK9e";
-    const extractedIDs = mediaPage[0].map(id => [id[0]]);
-    const resultString = JSON.stringify(extractedIDs);
+    productIdList = productIdList.map(id => [id]);
 
-    const requestData = [[[rpcids, `[[[${resultString}],[[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[],null,null,null,null,null,null,null,null,null,null,[]]]]]`, null, "generic"]]];
+    const requestData = [[[rpcids, `[[[${JSON.stringify(productIdList)}],[[null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,[],null,null,null,null,null,null,null,null,null,null,[]]]]]`, null, "generic"]]];
 
     try {
         const response = await makeApiRequest(rpcids, requestData);
@@ -199,7 +268,7 @@ const getMediaSizeAPI = async (mediaPage) => {
 
 const moveMediaToTrashAPI = async (mediaIdList) => {
     const rpcids = "XwAOJf";
-    const requestData = [[[rpcids, `[null,1,${JSON.stringify(mediaIdList)},3]`, null, "generic"]]];
+    const requestData = [[[rpcids, JSON.stringify([null, 1, mediaIdList, 3]), null, "generic"]]];
     try {
         const response = await makeApiRequest(rpcids, requestData);
         return response[0][2];
@@ -210,15 +279,17 @@ const moveMediaToTrashAPI = async (mediaIdList) => {
 };
 
 async function logForTesting() {
-    const mediaData = await addMediaToAlbum("newalbum1", ["AF1QipOu7e1cFa_SCsPgEBaBfhuvlR8ouj-G4EFxbNw","AF1QipPn_s9KnBp86EFUh3aDsj0vl06IVpXQHmn5AyE", "AF1QipMqTOLq6pDeqMsioQZ-aU6urM-fCGVuw9mfsOA"]);
-    console.log('addMediaToaNewAlbum:', mediaData);
+    // const trashPageId = "AH_uQ40j4o9d1CxS6pIcSMJX7hbC73hvC_CL62PcZ0_GOEr4i_xAb-fLhaHB1B-dD7VNlZjeKUwC1ESIOdIyAIzWu04BuM67RuouYsLF8DXGKK15tMNHB-qRFEIe4jKKTOfB_ubrAvlS"
+    // const trash = await getTrashItemsAPI(trashPageId)
+    const mediaData = await addMediaToAlbum("newalbum1", ["AF1QipOu7e1cFa_SCsPgEBaBfhuvlR8ouj-G4EFxbNw", "AF1QipPn_s9KnBp86EFUh3aDsj0vl06IVpXQHmn5AyE", "AF1QipMqTOLq6pDeqMsioQZ-aU6urM-fCGVuw9mfsOA"]);
+    console.log('newalbum1:', mediaData);
 }
 
 const deleteMediaTakingUpSpace = async () => {
     let nextTimestamp = null;
     console.log("deleteMediaTakingUpSpace")
     while (true) {
-        const mediaPage = await getMediaAPI(nextTimestamp)
+        const mediaPage = await getMediaPageAPI(nextTimestamp)
         nextTimestamp = parseInt(mediaPage[2]) - 1
         console.log('getMedia:', mediaPage);
         if (mediaPage && !mediaPage[0]) {
@@ -231,9 +302,16 @@ const deleteMediaTakingUpSpace = async () => {
         console.log("page deleted")
     }
 }
-logForTesting()
+//logForTesting()
 
 
-window.logForTesting = logForTesting;
+window.restoreAllFromTrash = restoreAllFromTrash;
+window.getMediaSizeAPI = getMediaSizeAPI;
+window.moveMediaToTrashAPI = moveMediaToTrashAPI;
+window.getMediaPageAPI = getMediaPageAPI;
+window.addMediaToAlbumAPI = addMediaToAlbumAPI;
+window.identifyMediaOccupingSpace = identifyMediaOccupingSpace;
+window.trashAllMediaInAlbum = trashAllMediaInAlbum;
+
 //deleteMediaTakingUpSpace()
 

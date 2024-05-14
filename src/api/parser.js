17184afe3a3ee7c784822c780d19a43072447457
xob0t <32616886+xob0t@users.noreply.github.com>
2024-05-14 15:12:39 +0300

    isLivePhoto media property

--- src/api/parser.js
+++ src/api/parser.js
@@ -10,6 +10,7 @@ export default function parser(data, rpcid) {
       isFavorite: rawItemData?.at(-1)?.[163238866]?.[0],
       duration: rawItemData?.at(-1)?.[76647426]?.[0],
       descriptionShort: rawItemData?.at(-1)?.[396644657]?.[0],
+      isLivePhoto: rawItemData?.at(-1)?.[146008172] ? true : false,
       isOwned: rawItemData?.[7]?.[12]?.[0] !== 27
     };
   }

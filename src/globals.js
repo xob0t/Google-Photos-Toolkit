b63bd97e5d7d650a7869767ad992abfc2250708a
xob0t <32616886+xob0t@users.noreply.github.com>
2024-06-16 01:27:29 +0300

    feat: expose gptk api

--- src/globals.js
+++ src/globals.js
@@ -1,5 +1,11 @@
+import Api from './api/api.js';
 import ApiUtils from './api/api-utils.js';
 import Core from './gptk-core.js';
 
 export const core = new Core();
 export const apiUtils = new ApiUtils(core);
+
+// exposing api to be accesible globally
+window.gptkApi = new Api();
+window.gptkCore = core;
+window.gptkApiUtils = apiUtils;

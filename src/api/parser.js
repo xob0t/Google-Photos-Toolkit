1449fbe4b3e24285d3b212f5e71e67f4018f2672
xob0t <32616886+xob0t@users.noreply.github.com>
2024-07-31 17:06:23 +0300

    chore: better naming consistency

--- src/api/parser.js
+++ src/api/parser.js
@@ -4,39 +4,35 @@ export default function parser(data, rpcid) {
 
   add =w417-h174-k-no?authuser=0 to thumbnail url to set custon size, remove 'video' watermark, remove auth requirement
 
-  TODO:
-  make all functions use the same convetion for naming `data` and `rawItemData`
   */
 
-
-
-  function libraryItemParse(rawItemData) {
+  function libraryItemParse(itemData) {
     return {
-      mediaKey: rawItemData?.[0],
-      timestamp: rawItemData?.[2],
-      timezoneOffset: rawItemData?.[4],
-      creationTimestamp: rawItemData?.[5],
-      dedupKey: rawItemData?.[3],
-      thumb: rawItemData?.[1]?.[0],
-      resWidth: rawItemData?.[1]?.[1],
-      resHeight: rawItemData?.[1]?.[2],
-      isArchived: rawItemData?.[13],
-      isFavorite: rawItemData?.at(-1)?.[163238866]?.[0],
-      duration: rawItemData?.at(-1)?.[76647426]?.[0],
-      descriptionShort: rawItemData?.at(-1)?.[396644657]?.[0],
-      isLivePhoto: rawItemData?.at(-1)?.[146008172] ? true : false,
-      livePhotoDuration: rawItemData?.at(-1)?.[146008172]?.[1],
-      isOwned: rawItemData[7]?.filter((subArray) => subArray.includes(27)).length === 0,
+      mediaKey: itemData?.[0],
+      timestamp: itemData?.[2],
+      timezoneOffset: itemData?.[4],
+      creationTimestamp: itemData?.[5],
+      dedupKey: itemData?.[3],
+      thumb: itemData?.[1]?.[0],
+      resWidth: itemData?.[1]?.[1],
+      resHeight: itemData?.[1]?.[2],
+      isArchived: itemData?.[13],
+      isFavorite: itemData?.at(-1)?.[163238866]?.[0],
+      duration: itemData?.at(-1)?.[76647426]?.[0],
+      descriptionShort: itemData?.at(-1)?.[396644657]?.[0],
+      isLivePhoto: itemData?.at(-1)?.[146008172] ? true : false,
+      livePhotoDuration: itemData?.at(-1)?.[146008172]?.[1],
+      isOwned: itemData[7]?.filter((subArray) => subArray.includes(27)).length === 0,
       geoLocation: {
-        coordinates: rawItemData?.at(-1)?.[129168200]?.[1]?.[0],
-        name: rawItemData?.at(-1)?.[129168200]?.[1]?.[4]?.[0]?.[1]?.[0]?.[0],
+        coordinates: itemData?.at(-1)?.[129168200]?.[1]?.[0],
+        name: itemData?.at(-1)?.[129168200]?.[1]?.[4]?.[0]?.[1]?.[0]?.[0],
       },
     };
   }
 
   function libraryTimelinePage(data) {
     return {
-      items: data?.[0]?.map((rawItemData) => libraryItemParse(rawItemData)),
+      items: data?.[0]?.map((itemData) => libraryItemParse(itemData)),
       nextPageId: data?.[1],
       lastItemTimestamp: parseInt(data?.[2]),
     };
@@ -44,107 +40,107 @@ export default function parser(data, rpcid) {
 
   function libraryGenericPage(data) {
     return {
-      items: data?.[0]?.map((rawItemData) => libraryItemParse(rawItemData)),
+      items: data?.[0]?.map((itemData) => libraryItemParse(itemData)),
       nextPageId: data?.[1],
     };
   }
 
-  function lockedFolderItemParse(rawItemData) {
+  function lockedFolderItemParse(itemData) {
     return {
-      mediaKey: rawItemData?.[0],
-      timestamp: rawItemData?.[2],
-      creationTimestamp: rawItemData?.[5],
-      dedupKey: rawItemData?.[3],
-      duration: rawItemData?.at(-1)?.[76647426]?.[0],
+      mediaKey: itemData?.[0],
+      timestamp: itemData?.[2],
+      creationTimestamp: itemData?.[5],
+      dedupKey: itemData?.[3],
+      duration: itemData?.at(-1)?.[76647426]?.[0],
     };
   }
 
   function lockedFolderPage(data) {
     return {
       nextPageId: data?.[0],
-      items: data?.[1]?.map((rawItemData) => lockedFolderItemParse(rawItemData)),
+      items: data?.[1]?.map((itemData) => lockedFolderItemParse(itemData)),
     };
   }
 
-  function linkParse(rawLinkData) {
+  function linkParse(itemData) {
     return {
-      mediaKey: rawLinkData?.[6],
-      linkId: rawLinkData?.[17],
-      itemCount: rawLinkData?.[3],
+      mediaKey: itemData?.[6],
+      linkId: itemData?.[17],
+      itemCount: itemData?.[3],
     };
   }
 
   function linksPage(data) {
     return {
-      items: data?.[0]?.map((rawLinkData) => linkParse(rawLinkData)),
+      items: data?.[0]?.map((itemData) => linkParse(itemData)),
       nextPageId: data?.[1],
     };
   }
 
-  function albumParse(rawAlbumData) {
+  function albumParse(itemData) {
     return {
-      mediaKey: rawAlbumData?.[0],
-      viewerActorId: rawAlbumData?.[6]?.[0],
-      title: rawAlbumData?.at(-1)?.[72930366]?.[1],
-      thumb: rawAlbumData?.[1]?.[0],
-      itemCount: rawAlbumData?.at(-1)?.[72930366]?.[3],
-      creationTimestamp: rawAlbumData?.at(-1)?.[72930366]?.[2]?.[4],
-      modifiedTimestamp: rawAlbumData?.at(-1)?.[72930366]?.[2]?.[9],
-      timestampRange: [rawAlbumData?.at(-1)?.[72930366]?.[2]?.[5], rawAlbumData?.at(-1)?.[72930366]?.[2]?.[6]],
-      isShared: rawAlbumData?.at(-1)?.[72930366]?.[4] || false,
+      mediaKey: itemData?.[0],
+      viewerActorId: itemData?.[6]?.[0],
+      title: itemData?.at(-1)?.[72930366]?.[1],
+      thumb: itemData?.[1]?.[0],
+      itemCount: itemData?.at(-1)?.[72930366]?.[3],
+      creationTimestamp: itemData?.at(-1)?.[72930366]?.[2]?.[4],
+      modifiedTimestamp: itemData?.at(-1)?.[72930366]?.[2]?.[9],
+      timestampRange: [itemData?.at(-1)?.[72930366]?.[2]?.[5], itemData?.at(-1)?.[72930366]?.[2]?.[6]],
+      isShared: itemData?.at(-1)?.[72930366]?.[4] || false,
     };
   }
 
   function albumsPage(data) {
     return {
-      items: data?.[0]?.map((rawAlbumData) => albumParse(rawAlbumData)),
+      items: data?.[0]?.map((itemData) => albumParse(itemData)),
       nextPageId: data?.[1],
     };
   }
 
-  function albumItemParse(rawItemData) {
+  function albumItemParse(itemData) {
     return {
-      mediaKey: rawItemData?.[0],
-      thumb: rawItemData?.[1]?.[0],
-      resWidth: rawItemData[1]?.[1],
-      resHeight: rawItemData[1]?.[2],
-      timestamp: rawItemData?.[2],
-      timezoneOffset: rawItemData?.[4],
-      creationTimestamp: rawItemData?.[5],
-      dedupKey: rawItemData?.[3],
-      isLivePhoto: rawItemData?.at(-1)?.[146008172] ? true : false,
-      livePhotoDuration: rawItemData?.at(-1)?.[146008172]?.[1],
-      duration: rawItemData?.at(-1)?.[76647426]?.[0],
+      mediaKey: itemData?.[0],
+      thumb: itemData?.[1]?.[0],
+      resWidth: itemData[1]?.[1],
+      resHeight: itemData[1]?.[2],
+      timestamp: itemData?.[2],
+      timezoneOffset: itemData?.[4],
+      creationTimestamp: itemData?.[5],
+      dedupKey: itemData?.[3],
+      isLivePhoto: itemData?.at(-1)?.[146008172] ? true : false,
+      livePhotoDuration: itemData?.at(-1)?.[146008172]?.[1],
+      duration: itemData?.at(-1)?.[76647426]?.[0],
     };
   }
 
-  function trashItemParse(rawItemData) {
+  function trashItemParse(itemData) {
     return {
-      mediaKey: rawItemData?.[0],
-      thumb: rawItemData?.[1]?.[0],
-      resWidth: rawItemData?.[1]?.[1],
-      resHeight: rawItemData?.[1]?.[2],
-      timestamp: rawItemData?.[2],
-      timezoneOffset: rawItemData?.[4],
-      creationTimestamp: rawItemData?.[5],
-      dedupKey: rawItemData?.[3],
-      duration: rawItemData?.at(-1)?.[76647426]?.[0],
+      mediaKey: itemData?.[0],
+      thumb: itemData?.[1]?.[0],
+      resWidth: itemData?.[1]?.[1],
+      resHeight: itemData?.[1]?.[2],
+      timestamp: itemData?.[2],
+      timezoneOffset: itemData?.[4],
+      creationTimestamp: itemData?.[5],
+      dedupKey: itemData?.[3],
+      duration: itemData?.at(-1)?.[76647426]?.[0],
     };
   }
 
-  function actorParse(rawActorData) {
+  function actorParse(data) {
     return {
-      actorId: rawActorData?.[0],
-      gaiaId: rawActorData?.[1],
-      name: rawActorData?.[11]?.[0],
-      gender: rawActorData?.[11]?.[2],
-      profiePhotoUrl: rawActorData?.[12]?.[0],
+      actorId: data?.[0],
+      gaiaId: data?.[1],
+      name: data?.[11]?.[0],
+      gender: data?.[11]?.[2],
+      profiePhotoUrl: data?.[12]?.[0],
     };
   }
 
   function albumItemsPage(data) {
     return {
-      items: data?.[1]?.map((rawItemData) => albumItemParse(rawItemData)),
+      items: data?.[1]?.map((itemData) => albumItemParse(itemData)),
       nextPageId: data?.[2],
       mediaKey: data?.[3][0],
       title: data?.[3][1],
@@ -156,33 +152,33 @@ export default function parser(data, rpcid) {
       totalItemCount: data?.[3][2][21],
       authKey: data?.[3][19],
       owner: actorParse(data?.[3][5]),
-      members: data?.[3][9]?.map((memberData) => actorParse(memberData)),
+      members: data?.[3][9]?.map((itemData) => actorParse(itemData)),
     };
   }
 
   function trashPage(data) {
     return {
-      items: data?.[0].map((rawItemData) => trashItemParse(rawItemData)),
+      items: data?.[0].map((itemData) => trashItemParse(itemData)),
       nextPageId: data?.[1],
     };
   }
 
-  function itemBulkMediaInfoParse(rawItemData) {
+  function itemBulkMediaInfoParse(itemData) {
     return {
-      mediaKey: rawItemData?.[0],
-      descriptionFull: rawItemData?.[1]?.[2],
-      fileName: rawItemData?.[1]?.[3],
-      timestamp: rawItemData?.[1]?.[6],
-      timezoneOffset: rawItemData?.[1]?.[7],
-      creationTimestamp: rawItemData?.[1]?.[8],
-      size: rawItemData?.[1]?.[9],
-      takesUpSpace: rawItemData?.[1]?.at(-1)?.[0] === undefined ? null : rawItemData?.[1]?.at(-1)?.[0] === 1,
-      spaceTaken: rawItemData?.[1]?.at(-1)?.[1],
-      isOriginalQuality: rawItemData?.[1]?.at(-1)?.[2] === undefined ? null : rawItemData?.[1]?.at(-1)?.[2] === 2,
+      mediaKey: itemData?.[0],
+      descriptionFull: itemData?.[1]?.[2],
+      fileName: itemData?.[1]?.[3],
+      timestamp: itemData?.[1]?.[6],
+      timezoneOffset: itemData?.[1]?.[7],
+      creationTimestamp: itemData?.[1]?.[8],
+      size: itemData?.[1]?.[9],
+      takesUpSpace: itemData?.[1]?.at(-1)?.[0] === undefined ? null : itemData?.[1]?.at(-1)?.[0] === 1,
+      spaceTaken: itemData?.[1]?.at(-1)?.[1],
+      isOriginalQuality: itemData?.[1]?.at(-1)?.[2] === undefined ? null : itemData?.[1]?.at(-1)?.[2] === 2,
     };
   }
 
-  function itemInfoExtParse(rawItemData) {
+  function itemInfoExtParse(itemData) {
     const source = [];
 
     const sourceMap = {
@@ -195,82 +191,82 @@ export default function parser(data, rpcid) {
       11: 'gmail',
     };
 
-    source[0] = rawItemData[0]?.[27]?.[0] ? sourceMap[rawItemData[0][27][0]] : null;
+    source[0] = itemData[0]?.[27]?.[0] ? sourceMap[itemData[0][27][0]] : null;
 
     const sourceMapSecondary = {
       1: 'android',
       3: 'ios',
     };
 
-    source[1] = rawItemData[0]?.[27]?.[1]?.[2] ? sourceMapSecondary[rawItemData[0][27][1][2]] : null;
+    source[1] = itemData[0]?.[27]?.[1]?.[2] ? sourceMapSecondary[itemData[0][27][1][2]] : null;
 
     // this is a mess, for some items it is in 27, for some in 28
     // for now it is better to just ignore it and use info from itemInfo, it is much more reliable
     // let owner = null;
-    // if (rawItemData[0]?.[27]?.length > 0){
-    //   owner = actorParse(rawItemData[0]?.[27]?.[3]?.[0]?.[11]?.[0] || rawItemData[0]?.[27]?.[4]?.[0]?.[11]?.[0]);
+    // if (itemData[0]?.[27]?.length > 0){
+    //   owner = actorParse(itemData[0]?.[27]?.[3]?.[0]?.[11]?.[0] || itemData[0]?.[27]?.[4]?.[0]?.[11]?.[0]);
     // }
     // if (!owner){
-    //   owner = actorParse(rawItemData[0]?.[28]);
+    //   owner = actorParse(itemData[0]?.[28]);
     // }
 
     return {
-      mediaKey: rawItemData[0]?.[0],
-      dedupKey: rawItemData[0]?.[11],
-      descriptionFull: rawItemData[0]?.[1],
-      fileName: rawItemData[0]?.[2],
-      timestamp: rawItemData[0]?.[3],
-      timezoneOffset: rawItemData[0]?.[4],
-      size: rawItemData[0]?.[5],
-      resWidth: rawItemData[0]?.[6],
-      resHeight: rawItemData[0]?.[7],
-      cameraInfo: rawItemData[0]?.[23],
-      albums: rawItemData[0]?.[19]?.map((rawItemData) => albumParse(rawItemData)),
+      mediaKey: itemData[0]?.[0],
+      dedupKey: itemData[0]?.[11],
+      descriptionFull: itemData[0]?.[1],
+      fileName: itemData[0]?.[2],
+      timestamp: itemData[0]?.[3],
+      timezoneOffset: itemData[0]?.[4],
+      size: itemData[0]?.[5],
+      resWidth: itemData[0]?.[6],
+      resHeight: itemData[0]?.[7],
+      cameraInfo: itemData[0]?.[23],
+      albums: itemData[0]?.[19]?.map((itemData) => albumParse(itemData)),
       source: source,
-      takesUpSpace: rawItemData[0]?.[30]?.[0] === undefined ? null : rawItemData[0]?.[30]?.[0] === 1,
-      spaceTaken: rawItemData[0]?.[30]?.[1],
-      isOriginalQuality: rawItemData[0]?.[30]?.[2] === undefined ? null : rawItemData[0][30][2] === 2,
-      savedToYourPhotos: rawItemData[0]?.[12].filter((subArray) => subArray.includes(20)).length === 0,
+      takesUpSpace: itemData[0]?.[30]?.[0] === undefined ? null : itemData[0]?.[30]?.[0] === 1,
+      spaceTaken: itemData[0]?.[30]?.[1],
+      isOriginalQuality: itemData[0]?.[30]?.[2] === undefined ? null : itemData[0][30][2] === 2,
+      savedToYourPhotos: itemData[0]?.[12].filter((subArray) => subArray.includes(20)).length === 0,
       // owner: owner,
       geoLocation: {
-        coordinates: rawItemData[0]?.[9]?.[0] || rawItemData[0]?.[13]?.[0],
-        name: rawItemData[0]?.[13]?.[2]?.[0]?.[1]?.[0]?.[0],
-        mapThumb: rawItemData?.[1],
+        coordinates: itemData[0]?.[9]?.[0] || itemData[0]?.[13]?.[0],
+        name: itemData[0]?.[13]?.[2]?.[0]?.[1]?.[0]?.[0],
+        mapThumb: itemData?.[1],
       },
-      other: rawItemData[0]?.[31],
+      other: itemData[0]?.[31],
     };
   }
 
-  function itemInfoParse(rawItemData) {
+  function itemInfoParse(itemData) {
     return {
-      mediaKey: rawItemData[0]?.[0],
-      dedupKey: rawItemData[0]?.[3],
-      resWidth: rawItemData[0]?.[1]?.[1],
-      resHeight: rawItemData[0]?.[1]?.[2],
-      timestamp: rawItemData[0]?.[2],
-      owner: actorParse(rawItemData[3]),
-      timezoneOffset: rawItemData[0]?.[4],
-      creationTimestamp: rawItemData[0]?.[5],
-      downloadUrl: rawItemData?.[1],
-      downloadOriginalUrl: rawItemData?.[7], // url to download the original if item was modified after the upload
-      savedToYourPhotos: rawItemData[0]?.[15]?.[163238866]?.length > 0,
-      isArchived: rawItemData[0]?.[13],
-      takesUpSpace: rawItemData[0]?.[15]?.[318563170]?.[0]?.[0] === undefined ? null : rawItemData[0]?.[15]?.[318563170]?.[0]?.[0] === 1,
-      spaceTaken: rawItemData[0]?.[15]?.[318563170]?.[0]?.[1],
-      isOriginalQuality: rawItemData[0]?.[15]?.[318563170]?.[0]?.[2] === undefined ? null : rawItemData[0]?.[15]?.[318563170]?.[0]?.[2] === 2,
-      isFavorite: rawItemData[0]?.[15]?.[163238866]?.[0],
-      duration: rawItemData[0]?.[15]?.[76647426]?.[0],
-      isLivePhoto: rawItemData[0]?.[15]?.[146008172] ? true : false,
-      livePhotoDuration: rawItemData[0]?.[15]?.[146008172]?.[1],
-      livePhotoVideoDownloadUrl: rawItemData[0]?.[15]?.[146008172]?.[3],
-      trashTimestamp: rawItemData[0]?.[15]?.[225032867]?.[0],
-      descriptionFull: rawItemData[10],
-      thumb: rawItemData[12],
+      mediaKey: itemData[0]?.[0],
+      dedupKey: itemData[0]?.[3],
+      resWidth: itemData[0]?.[1]?.[1],
+      resHeight: itemData[0]?.[1]?.[2],
+      timestamp: itemData[0]?.[2],
+      owner: actorParse(itemData[3]),
+      timezoneOffset: itemData[0]?.[4],
+      creationTimestamp: itemData[0]?.[5],
+      downloadUrl: itemData?.[1],
+      downloadOriginalUrl: itemData?.[7], // url to download the original if item was modified after the upload
+      savedToYourPhotos: itemData[0]?.[15]?.[163238866]?.length > 0,
+      isArchived: itemData[0]?.[13],
+      takesUpSpace: itemData[0]?.[15]?.[318563170]?.[0]?.[0] === undefined ? null : itemData[0]?.[15]?.[318563170]?.[0]?.[0] === 1,
+      spaceTaken: itemData[0]?.[15]?.[318563170]?.[0]?.[1],
+      isOriginalQuality: itemData[0]?.[15]?.[318563170]?.[0]?.[2] === undefined ? null : itemData[0]?.[15]?.[318563170]?.[0]?.[2] === 2,
+      isFavorite: itemData[0]?.[15]?.[163238866]?.[0],
+      duration: itemData[0]?.[15]?.[76647426]?.[0],
+      isLivePhoto: itemData[0]?.[15]?.[146008172] ? true : false,
+      livePhotoDuration: itemData[0]?.[15]?.[146008172]?.[1],
+      livePhotoVideoDownloadUrl: itemData[0]?.[15]?.[146008172]?.[3],
+      trashTimestamp: itemData[0]?.[15]?.[225032867]?.[0],
+      descriptionFull: itemData[10],
+      thumb: itemData[12],
     };
   }
 
   function bulkMediaInfo(data) {
-    return data.map((rawItemData) => itemBulkMediaInfoParse(rawItemData));
+    return data.map((itemData) => itemBulkMediaInfoParse(itemData));
   }
 
   function downloadTokenCheckParse(data) {
@@ -284,30 +280,30 @@ export default function parser(data, rpcid) {
 
   function storageQuotaParse(data){
     return{
-      totalUsed: data[6][0],
-      totalAvailable: data[6][1],
-      usedByGPhotos: data[6][3],
+      totalUsed: data?.[6]?.[0],
+      totalAvailable: data?.[6]?.[1],
+      usedByGPhotos: data?.[6]?.[3],
     };
   }
 
-  function remoteMatchParse(data){
+  function remoteMatchParse(itemData){
     return{
-      hash: data[0],
-      mediaKey: data[1][0],
-      thumb: data[1][1][0],
-      resWidth: data[1][1][1],
-      resHeight: data[1][1][2],
-      timestamp: data[1][2],
-      dedupKey: data[1][3],
-      timezoneOffset: data[1][4],
-      creationTimestamp: data[1][5],
-      duration: data[1]?.at(-1)?.[76647426]?.[0],
-      cameraInfo: data[1][1][8],
+      hash: itemData?.[0],
+      mediaKey: itemData?.[1]?.[0],
+      thumb: itemData?.[1]?.[1]?.[0],
+      resWidth: itemData?.[1]?.[1]?.[1],
+      resHeight: itemData?.[1]?.[1]?.[2],
+      timestamp: itemData?.[1]?.[2],
+      dedupKey: itemData?.[1]?.[3],
+      timezoneOffset: itemData?.[1]?.[4],
+      creationTimestamp: itemData?.[1]?.[5],
+      duration: itemData?.[1]?.at(-1)?.[76647426]?.[0],
+      cameraInfo: itemData?.[1]?.[1]?.[8],
     };
   }
 
   function remoteMatchesParse(data) {
-    return data[0].map((rawItemData) => remoteMatchParse(rawItemData));
+    return data[0].map((itemData) => remoteMatchParse(itemData));
   }
 
   if (!data?.length) return null;
